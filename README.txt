c2da73b09c62c96d7f6c1f262325f2bf13922458
hofmockel <dreagonfly@gmx.de>
2011-11-28 12:04:53 +0100

    Initial commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+# Integrate it like this
+<Plugin python>
+    ModulePath "..../collectd_plugins/beanstalkd"
+    Import "beanstalkd"
+    <Module beanstalkd>
+        Host "127.0.0.1"
+        Port "11300"
+        # only monitor tubes where tubename start with the following prefixes
+        tubes_prefix "inventory default"
+    </Module>
+</Plugin>
---
+++ beanstalkd.py
@@ -0,0 +1,59 @@
+#
+# Plugin to collectd statistics from beanstalkd
+#
+
+import collectd
+from beanstalkc import Connection
+
+class Beanstalk(object):
+
+    def __init__(self):
+        self.plugin_name = "beanstalkd"
+        self.host = '127.0.0.1'
+        self.port = 11300
+        self.tubes_prefix = ['default']
+
+    def submit(self, type, instance, value, tube=None):
+        if tube:
+            plugin_instance = '%s-%s' % (self.port, tube)
+        else:
+            plugin_instance = str(self.port)
+
+        v = collectd.Values()
+        v.plugin = self.plugin_name
+        v.plugin_instance = plugin_instance
+        v.type = type
+        v.type_instance = instance
+        v.values = [value, ]
+        v.dispatch()
+
+    def do_server_status(self):
+        conn = Connection(self.host, self.port)
+        srv_stats = conn.stats()
+
+        for cmd in ('put', 'reserve-with-timeout', 'delete'):
+            self.submit('counter', cmd, srv_stats['cmd-%s' % cmd])
+        self.submit('counter', 'total_jobs', srv_stats['total-jobs'])
+        for tube in conn.tubes():
+            for prefix in self.tubes_prefix:
+                if tube.startswith(prefix):
+                    tube_stats = conn.stats_tube(tube)
+                    self.submit('records', 'current_ready', tube_stats['current-jobs-ready'], tube)
+                    self.submit('counter', 'total_jobs', tube_stats['total-jobs'], tube)
+
+        conn.close()
+
+    def config(self, obj):
+        for node in obj.children:
+            if node.key == 'Port':
+                self.port = int(node.values[0])
+            elif node.key == 'Host':
+                self.host = node.values[0]
+            elif node.key == 'tubes_prefix':
+                self.tubes_prefix = node.values
+            else:
+                collectd.warning("beanstalkd plugin: Unkown configuration key %s" % node.key)
+
+beanstalkd = Beanstalk()
+collectd.register_read(beanstalkd.do_server_status)
+collectd.register_config(beanstalkd.config)
